1b171ae2e5752b368c61983f171d04d7e7974ba0
hubing <1349040796@qq.com>
2020-03-13 19:27:11 +0800

    收货地址

--- piluwa/src/components/My/myaddress.vue
+++ piluwa/src/components/My/myaddress.vue
@@ -11,25 +11,27 @@
         </div>
 
         <!-- 收获地址信息 -->
-        <nav id="addressList">
-            <li v-for="(item,index) in addressList" :key="index">
-                <p>
-                    <span>{{item.getName}}</span>
-                    <span>{{item.getPhone}}</span>
-                </p>
-                <p>
-                   <span>{{item.address.replace('-',' ')}}</span>
-                </p>
-                <div class="edit" :addressId='item.addressId' @click="editaddress(item.addressId)" >
-                    <van-icon   size="0.15rem" color="#f58232" name="records" />
-                </div>
-            </li>
-            
-
-        </nav>
+        <div class='wrapper' ref='Wrapper'>
+        <div class='content'>
+            <nav id="addressList">
+                <li v-for="(item,index) in addressList" :key="index">
+                    <p>
+                        <span>{{item.getName}}</span>
+                        <span>{{item.getPhone}}</span>
+                    </p>
+                    <p>
+                    <span>{{item.address.replace('-',' ')}}</span>
+                    </p>
+                    <div class="edit" :addressId='item.addressId' @click="editaddress(item.addressId)" >
+                        <van-icon   size="0.15rem" color="#f58232" name="records" />
+                    </div>
+                </li>
+            </nav>
+        </div>
+        </div>
 
         <!-- 添加收货地址 -->
-        <router-link tag="div" id="add" to="addaddress/add">
+        <router-link ref="addaddress" tag="div" id="add" to="addaddress/add">
             <van-icon name="plus"  color="#f38339"/>
             <span>添加收获地址</span>
         </router-link>
@@ -44,6 +46,7 @@
 
 
 <script>
+import BS from 'better-scroll'
 import {mapState} from 'vuex'
 import {getAddress} from 'api/api.js'
 export default {
@@ -62,14 +65,21 @@ export default {
        editaddress(Id){  //把ID传入地址
         this.$router.push(`addaddress/${Id}`)
             console.log(Id)
-       }
+       },
+        initBs(){
+            let wrapper = this.$refs.Wrapper
+            this.Bs = new BS(wrapper,{probeType:3,click:true})
+        },
     },
     mounted(){
         let obj={token:this.userMsg.token}
         getAddress(obj)
         .then((res)=>{
             console.log(res.result.resArr)
-            this.addressList=res.result.resArr;    
+            this.addressList=res.result.resArr;  
+            this.$nextTick(()=>{
+                this.initBs()
+            })  
         })
     }
 }
@@ -88,33 +98,42 @@ export default {
            .head()
         }
     }
-    #addressList{
-        width: 100%;
-        li{
-            width: 100%;
-            height: 0.96rem;
-            background: #fff;
-            padding: 0 0.15rem;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            font-size: 0.14rem;
-            font-weight: 500;
-            border-bottom: 1px solid gray;
-            position: relative;
-            p{
-                span{
-                    display: inline-block;
-                    margin: 0 0.03rem;
+
+.wrapper{
+    width: 100%;
+    overflow: hidden;
+    background: #f7f7f7;   
+         .content{
+            #addressList{
+                width: 100%;
+                li{
+                    width: 100%;
+                    height: 0.96rem;
+                    background: #fff;
+                    padding: 0 0.15rem;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-around;
+                    font-size: 0.14rem;
+                    font-weight: 500;
+                    border-bottom: 1px solid gray;
+                    position: relative;
+                    p{
+                        span{
+                            display: inline-block;
+                            margin: 0 0.03rem;
+                        }
+                    }
+                    .edit{
+                        position: absolute;
+                        right: 0.2rem;
+                        top: 0.15rem;
+                    }
                 }
             }
-            .edit{
-                position: absolute;
-                right: 0.2rem;
-                top: 0.15rem;
-            }
-        }
-    }
+         }
+}
+            
     #add{
 
         width: 100%;
